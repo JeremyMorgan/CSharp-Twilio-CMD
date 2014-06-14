b8c51eb13cfcd8e7078ec8cd1f271e516d52e6cf
Jeremy Morgan <jeremymorgan@gmail.com>
2014-06-14 13:21:17 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-CSharp-Twilio-CMD
+Send text message from a command prompt with C# and Twilio
 =================
 
-Send a text message from the command prompt with Twilio
+This is for a series of applications I'm building using the Twilio service. 
+
+This sends a text message from the command prompt with Twilio.
+
+This is using the Twilio libraries available on NuGet
+
+Not sure how they could have made this any easier. 
+
+Sign up for your own account at [Twilio](https://www.twilio.com/user/account) and check it out. 
---
+++ Twiliotest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+
+namespace Twiliotest
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            // Find your Account Sid and Auth Token at twilio.com/user/account
+            string AccountSid = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            // your auth token
+            string AuthToken = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            // string to send message to
+            string sendto = "+1111111111";
+            // your Twilio Number 
+            string sendfrom = "+1111111111";
+            
+            // create a new instance of the client
+            var twilio = new TwilioRestClient(AccountSid, AuthToken);
+            // create a new message
+            var message = twilio.SendMessage(sendfrom, sendto, "This is a sample text message!", "");
+
+            // echo out the SID
+            Console.WriteLine("SID:" + message.Sid);
+            
+            // handle exception
+            if (message.RestException != null)
+            {
+                var error = message.RestException.Message;
+                Console.WriteLine(error.ToString());
+            }
+        }
+    }
+}
